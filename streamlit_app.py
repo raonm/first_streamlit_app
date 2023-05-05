4af7f57c27a1aaf46a6deaf273a6a3e3738b7ecb
raonm <rnuthakki@gmail.com>
2023-05-05 17:47:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My 'Mom's' new healthy diner') 
+streamlit.title("My Mom's new healthy diner") 
 streamlit.header('Breakfast Favorates')
 
 streamlit.text('🥣 Omega 3 Blueberry Oatmeal') 
