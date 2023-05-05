f2c6fae8b4510ecc17e42bc3b5f5e00202682733
raonm <rnuthakki@gmail.com>
2023-05-05 17:43:17 -0400

    Update streamlit_app.py
    Fixed images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner') 
 streamlit.header('Breakfast Favorates')
-🥣 🥗 🐔 🥑🍞
+
 streamlit.text('🥣 Omega 3 Blueberry Oatmeal') 
 streamlit.text('🥗 Kale, Spinach and Rocket smoothie') 
 streamlit.text('🐔 Hard boiled free-range eggs') 
