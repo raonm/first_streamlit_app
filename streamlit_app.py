848afd485333253617dfbded28c9d4e5b01565f5
raonm <rnuthakki@gmail.com>
2023-05-05 17:24:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner') 
+streamlit.header('Breakfast Menu') 
+streamlit.title('Omega 3 Blueberry Oatmeal') 
+streamlit.title('Kale, Spinach and Rocket smoothie') 
+streamlit.title('Hard boiled free-range eggs' ) 
