7112d777c90684617340dcc09f9437465ebd74ba
raonm <rnuthakki@gmail.com>
2023-05-05 17:42:51 -0400

    Update streamlit_app.py
    Added emogies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner') 
-streamlit.header('Breakfast Menu') 
-streamlit.text('Omega 3 Blueberry Oatmeal') 
-streamlit.text('Kale, Spinach and Rocket smoothie') 
-streamlit.text('Hard boiled free-range eggs') 
+streamlit.header('Breakfast Favorates')
+🥣 🥗 🐔 🥑🍞
+streamlit.text('🥣 Omega 3 Blueberry Oatmeal') 
+streamlit.text('🥗 Kale, Spinach and Rocket smoothie') 
+streamlit.text('🐔 Hard boiled free-range eggs') 
+streamlit.text('🥑🍞 avacado Toast') 
