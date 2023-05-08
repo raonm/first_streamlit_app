06f2ecf2c087d976db1c9d645c9a01a6c95fe4f4
raonm <rnuthakki@gmail.com>
2023-05-08 18:38:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,19 @@ except URLError as e:
     streamlit.error()
 # import requests
 
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.header("The Fruit Load List Contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
-# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.stop()
 # Take the json version of the data and normalizes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to screen as a table
@@ -55,11 +65,8 @@ streamlit.dataframe(fruityvice_normalized)
 # Don't run anything beyond this point
 streamlit.stop()
 
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
+
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
